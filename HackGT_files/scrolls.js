08a72914560892277486139b661800d9d5458fa6
dbotsford <3408480+dawsbot@users.noreply.github.com>
2014-10-03 14:42:39 -0600

    Ripped down HackGT to work from

---
+++ HackGT_files/scrolls.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+	var $window = $(window);
+
+	$window.scroll(function() {
+		if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+		 // some code..
+		}
+		else
+		{
+			
+			    if ($window.scrollTop() > $window.height())
+			    {
+			      $("#nav").css({
+			      	background: "rgba(0, 0, 0, 1)",
+			      	position: "fixed",
+			      	top: "0",
+			      	width: "100%",
+			      	// left: "20%",
+			      	height: "60px",
+			      	zIndex: "999"
+			      });
+			      $("#logo-container").css("margin-top", "231px");
+			      $("#bottomtext").css("padding-bottom", "200px");
+			    }
+			    if ($window.scrollTop() < $window.height())
+			    {
+					$("#nav").removeAttr("style");
+					$("#logo-container").removeAttr("style");
+					$("#bottomtext").removeAttr("style");
+
+			    }
+		}
+	});
+})
